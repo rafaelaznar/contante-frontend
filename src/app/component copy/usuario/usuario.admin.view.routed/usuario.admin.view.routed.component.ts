68f7d001c000e3af23f06d04cebdef9092eff22d
ABollullos <adrian.bollullos@gmail.com>
2024-11-11 18:52:50 +0100

    create // interface // model hecho

---
+++ src/app/component copy/usuario/usuario.admin.view.routed/usuario.admin.view.routed.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../../../service/usuario.service';
+import { IUsuario } from '../../../model/usuario.interface';
+
+
+@Component({
+  selector: 'app-usuario.admin.view.routed',
+  templateUrl: './usuario.admin.view.routed.component.html',
+  styleUrls: ['./usuario.admin.view.routed.component.css']
+})
+export class UsuarioAdminViewRoutedComponent implements OnInit {
+  id: number = 0;
+  route: string = '';
+  oUsuario: IUsuario = {
+    id: 0,
+    nombre: '',
+    apellido1: '',
+    apellido2: '',
+    email: '',
+  };
+  constructor(private oActivatedRoute: ActivatedRoute, private oUsuarioService: UsuarioService) { }
+
+  ngOnInit() {
+    this.id = this.oActivatedRoute.snapshot.params['id'];
+    this.getOne();
+  }
+  getOne() {
+    this.oUsuarioService.getOne(this.id).subscribe({
+      next: (data: IUsuario) => {
+        this.oUsuario = data;
+      },
+    });
+  }
+}
---
+++ src/app/model/tipoCuenta.interface.ts
@@ -0,0 +1,8 @@
+
+export interface ITipoCuenta {
+     id : number ;
+     descripcion : string ;
+     credito_o_debito : number ;
+     comentarios : string ;
+     real_o_nominal : number ;
+}
